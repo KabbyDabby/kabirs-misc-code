2b88655d4f6ca63f59e0e11def43cc3e4bddb04e
Kabir Goyal <kabirgoyal@icloud.com>
2025-08-11 22:30:15 -0400

    rust stuff

--- rust/learning_rust/enums/src/main.rs
+++ rust/learning_rust/enums/src/main.rs
@@ -1,13 +1,84 @@
 #[derive(Debug)]
-enum IpAddr {
-    V4(String),
-    V6(String),
+#[allow(unused)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WestVirginia,
+    Wisconsin,
+    Wyoming,
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
 
 fn main() {
-    let control_hub = IpAddr::V4(String::from("192.168.43.1"));
-    let home = IpAddr::V6(String::from("Hi"));
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter(UsState::Delaware);
 
-    println!("{control_hub:?}");
-    println!("{home:?}");
+    println!("{}", { value(penny) });
+    println!("{}", { value(nickel) });
+    println!("{}", { value(dime) });
+    println!("{}", { value(quarter) });
+}
+
+#[allow(unused)]
+fn value(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => 25,
+    }
 }
--- rust/learning_rust/structs/src/main.rs
+++ rust/learning_rust/structs/src/main.rs
@@ -5,13 +5,27 @@ struct User {
     sign_in_count: u64,
 }
 
-fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("Kubar"),
-        email: String::from("email@example.com"),
-        sign_in_count: 1,
-    };
+impl User {
+    fn new(active: bool, username: String, email: String, sign_in_count: u64) -> Self {
+        Self {
+            active,
+            username,
+            email,
+            sign_in_count,
+        }
+    }
+}
 
+fn main() {
+    let user1 = User::new(
+        false,
+        String::from("kubar"),
+        String::from("example@example.com"),
+        1,
+    );
+
+    println!("{}", user1.active);
     println!("{}", user1.username);
+    println!("{}", user1.email);
+    println!("{}", user1.sign_in_count);
 }
